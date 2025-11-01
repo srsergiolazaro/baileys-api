a37ff1ead64db75d5d1a7c010c14b589f6b87546
Sergio <sergiolazaromondargo@gmail.com>
2025-11-01 12:40:47 -0500

    Prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+// prettier.config.cjs
+
+/** @type {import("prettier").Config} */
+const config = {
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+	endOfLine: "lf",
+	semi: true,
+	singleQuote: false,
+};
+
+// Cambia esta línea
+module.exports = config;
--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	printWidth: 100,
-	tabWidth: 2,
-	useTabs: true,
-	endOfLine: "lf",
-	semi: true,
-	singleQuote: false,
-};
