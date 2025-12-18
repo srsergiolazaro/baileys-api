ef728955302de149b0f97c59710edd31516fa90e
root <root@srv849553.hstgr.cloud>
2025-12-18 12:50:57 +0000

    update ecosistem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,16 @@
 module.exports = {
-    apps: [
-        {
-            name: "baileys-api",
-            script: "dist/index.js",
-            instances: 1,
-            exec_mode: "fork",
-            autorestart: true,
-            watch: false,
-            env: {
-                NODE_ENV: "production",
-            },
-            post_update: [
-                "npm install",
-                "npm run build"
-            ]
-        }
-    ]
+  apps: [
+    {
+      name: "baileys-api",
+      script: "dist/index.js",
+      instances: 1,
+      exec_mode: "fork",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
+      env: {
+        NODE_ENV: "production",
+      }
+    }
+  ]
 }
