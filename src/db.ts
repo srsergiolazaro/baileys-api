d6e46b6b412d1ac62f7a9c0142e0aa0e0067114a
Sergio <sergiolazaromondargo@gmail.com>
2026-03-06 21:34:08 -0500

    no shutdown

--- src/db.ts
+++ src/db.ts
@@ -13,14 +13,4 @@ function createPrismaClient() {
 
 export const prisma = globalForPrisma.prisma || createPrismaClient();
 
-// Graceful shutdown
-const gracefulShutdown = async (signal: string) => {
-	console.log(`${signal} received, shutting down gracefully...`);
-	await prisma.$disconnect();
-	process.exit(0);
-};
-
-process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
-process.on('SIGINT', () => gracefulShutdown('SIGINT'));
-
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
